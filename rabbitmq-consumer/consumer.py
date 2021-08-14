ba90e74b11649543810291220125acfbf2d2b213
yiimingyang <yiimingyang@outlook.com>
2021-08-14 19:27:50 -0400

    correct some problems in consumer.py 2th

--- rabbitmq-consumer/consumer.py
+++ rabbitmq-consumer/consumer.py
@@ -11,6 +11,7 @@ def main():
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
+
     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')

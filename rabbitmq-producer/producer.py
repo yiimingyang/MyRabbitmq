7716b85ad40cfdc4c4dd94f1a773aa68ca82a318
yiimingyang <86273191+yiimingyang@users.noreply.github.com>
2021-08-16 11:20:38 -0400

    Update producer.py
    Change the message to send in producer.py

--- rabbitmq-producer/producer.py
+++ rabbitmq-producer/producer.py
@@ -6,6 +6,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello Workflow!')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello Workflow and action !')
 print(" [x] Sent 'Hello World!'")
 connection.close()

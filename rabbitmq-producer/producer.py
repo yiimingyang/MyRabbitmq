205b8f1f9f9430cea466eb2cc289afcdbca2639f
yiimingyang <yiimingyang@outlook.com>
2021-08-17 17:32:46 -0400

    change the message to be send

--- rabbitmq-producer/producer.py
+++ rabbitmq-producer/producer.py
@@ -6,6 +6,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello Teamwork yeah!')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello Teamwork yeah!!!')
 print(" [x] Sent 'Hello World!'")
 connection.close()

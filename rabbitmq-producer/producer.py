4d10fc0cd89ec23134926c0500a1cc76b5e656b4
yiimingyang <86273191+yiimingyang@users.noreply.github.com>
2021-08-18 21:58:54 -0400

    Update producer.py
    Try to use action: docker/build-and-push@v2 to push image to Docker Hub

--- rabbitmq-producer/producer.py
+++ rabbitmq-producer/producer.py
@@ -6,6 +6,6 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello Teamwork!')
+channel.basic_publish(exchange='', routing_key='hello', body='Hello Teamwork, build and push action!')
 print(" [x] Sent 'Hello World!'")
 connection.close()
